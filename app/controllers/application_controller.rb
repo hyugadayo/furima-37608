a71d8d812b566b75fa06dab3183c96b9284b03c7
hyugadayo <achano.0616@gmail.com>
2022-05-21 15:18:04 +0900

    Bassic認証の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_path
+
+  private
+  def basic_path
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_USER"] && password == ENV["BASIC_PASSWORD"]
+    end
+  end
 end
