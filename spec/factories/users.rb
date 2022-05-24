0a8756480f4159e45e67fc9a058fa14cb83b1203
hyugadayo <achano.0616@gmail.com>
2022-05-24 22:22:01 +0900

    FactoryBot作成・値作成

---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :user do
+    nickname  {Faker::Name.initials(number: 2)}
+    email  {Faker::Internet.free_email}
+    password  {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    last_name  {"山田"}
+    first_name {"太郎"}
+    last_name_kana{"ヤマダ"}
+    first_name_kana  {"タロウ"}
+    birth_date {Faker::Date.between(from: '2000-01-01', to: '2022-05-01')}
+
+  end
+end
