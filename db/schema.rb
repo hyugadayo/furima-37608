44c69d3f9fef20b7a34c1796419542546200846f
hyugadayo <achano.0616@gmail.com>
2022-05-29 14:42:13 +0900

    itemテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_22_123639) do
+ActiveRecord::Schema.define(version: 2022_05_29_053311) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "product", null: false
+    t.text "explain", null: false
+    t.integer "catefory_id", null: false
+    t.integer "state_id", null: false
+    t.integer "price", null: false
+    t.integer "delivery_cost_status_id", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "delivery_waiting_time_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -30,4 +45,5 @@ ActiveRecord::Schema.define(version: 2022_05_22_123639) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
