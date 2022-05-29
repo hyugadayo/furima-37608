35636aed27c12a4687f0ec6a980a7ecb54783520
hyugadayo <achano.0616@gmail.com>
2022-05-29 16:48:57 +0900

    stateモデル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,10 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :state
   belongs_to :user
   has_one_attached :image
 
   validates :category_id,nuwmericality: {other_than: 1, message: "can't be blank"} 
+  validates :state_id,nuwmericality: {other_than: 1, message: "can't be blank"}
 end
