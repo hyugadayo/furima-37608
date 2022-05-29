6ca5a1ba1cb5d8afb80502823331d86de4e5e9d1
hyugadayo <achano.0616@gmail.com>
2022-05-29 17:43:11 +0900

    delivery_cost_statusモデル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,9 +2,11 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :state
+  belongs_to :delivery_cost_status
   belongs_to :user
   has_one_attached :image
 
   validates :category_id,nuwmericality: {other_than: 1, message: "can't be blank"} 
   validates :state_id,nuwmericality: {other_than: 1, message: "can't be blank"}
+  validates :delivery_cost_status_id,nuwmericality: {other_than: 1, message: "can't be blank"}
 end
