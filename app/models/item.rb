3307c9720d8abac0866ab52c144724807dc41d75
hyugadayo <achano.0616@gmail.com>
2022-05-29 16:43:28 +0900

    categoryモデル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,8 @@
 class Item < ApplicationRecord
-
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
   belongs_to :user
   has_one_attached :image
+
+  validates :category_id,nuwmericality: {other_than: 1, message: "can't be blank"} 
 end
