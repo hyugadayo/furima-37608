5f86c30c6bda6a01dc94bb4c72485600ddd9d194
hyugadayo <achano.0616@gmail.com>
2022-05-29 18:26:36 +0900

    createアクション定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,5 +4,18 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
-  
+  def create
+    binding.pry
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:product,:explain,:category_id,:state_id,:price,:delivery_cost_status_id,:prefecture_id,:delivery_waiting_time_id).merge(user_id: current_user.id)
+  end
 end
