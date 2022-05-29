aa014be295a702a20e45cd712de92f9cb6dfb026
hyugadayo <achano.0616@gmail.com>
2022-05-29 17:52:39 +0900

    prefectureモデル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,10 +3,12 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :state
   belongs_to :delivery_cost_status
+  belongs_to :prefecture
   belongs_to :user
   has_one_attached :image
 
   validates :category_id,nuwmericality: {other_than: 1, message: "can't be blank"} 
   validates :state_id,nuwmericality: {other_than: 1, message: "can't be blank"}
   validates :delivery_cost_status_id,nuwmericality: {other_than: 1, message: "can't be blank"}
+  validates :prefecture_id,nuwmericality: {other_than: 0,message: "can't be blank"}
 end
