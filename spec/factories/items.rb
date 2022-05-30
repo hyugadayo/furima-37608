7684e14ab7570001ff971045023a0fda21c9fe5d
hyugadayo <achano.0616@gmail.com>
2022-05-30 23:33:30 +0900

    itemモデルのFactoryBot作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :item do
-    
+    product {商品名}
+    explain{商品名の説明}
+    category { Category.all.sample}
+    state { State.all.sample}
+    delivery_cost_status {DeliveryCostStatus.all.sample}
+    prefecture { Prefecture.all.sample}
+    delivery_waiting_time { DeliveryWaitingTime.all.sample}
+    price {Faker::Number.between(from: 300, to: 9999999)}
   end
 end
