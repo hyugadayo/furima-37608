db508546d4296c256351990e9ed1d1f9c3a5a478
hyugadayo <achano.0616@gmail.com>
2022-06-12 14:33:39 +0900

    purchaseコントローラー(create記入)

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,9 +1,20 @@
 class PurchasesController < ApplicationController
   def index
-    
+    @purchase_deliveryinformation = PurchaseDeliveryinformation.new
   end
 
   def create
-    
+    @purchase_deliveryinformation = PurchaseDeliveryinformation.new(purchase_params)
+    if @purchase_address.valid?
+      @purchase_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def purchase_params
+    params.require(:purchase_delivery).permit(:postal_code, :predecture, :address, :city, :building, :phone_number).merge(user_id: current_user.id, item_id: item.id)
   end
 end
