ca03e49f4a97822582208ac796911aaaf356759e
hyugadayo <achano.0616@gmail.com>
2022-06-12 17:37:28 +0900

    購入ボタンの表示有無の変更

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,4 +1,5 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!
   def index
     @purchase_deliveryinformation = PurchaseDeliveryinformation.new
     @item = Item.find(params[:item_id])
