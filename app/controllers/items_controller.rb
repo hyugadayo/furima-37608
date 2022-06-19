938d20ce0ae2e0567bbd82c62f5bf3b2187102f6
hyugadayo <achano.0616@gmail.com>
2022-06-19 12:31:52 +0900

    itemコントローラー・リファクタリング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,8 +22,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    redirect_to root_path if Purchase.exists?(item_id: @item.id)
-    redirect_to root_path unless current_user.id == @item.user_id
+    redirect_to root_path if !(current_user.id == @item.user_id) || @item.purchase.present?
   end
 
   def update
