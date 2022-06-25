cbd8e4d49361c89ed7eeed67aba88db8e9acc779
hyugadayo <achano.0616@gmail.com>
2022-06-26 00:13:20 +0900

    プレビュー複数表示・削除ボタン実装

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -5,36 +5,95 @@ document.addEventListener('DOMContentLoaded',function(){
   const previewList = document.getElementById('previews')
   // 新規投稿・編集ページのフォームがないならここで終了。「!」は論理否定演算子。
   if (!postForm) return null;
-  console.log("preview.jsが読み込まれました");
-
-  // input要素を取得
-  const fileField = document.querySelector('input[type="file"][name="item[image]"]');
-  // input要素に変化が起きた際に呼び出される関数
-  fileField.addEventListener('change', function(e){
-    // 古いプレビューが存在する場合は消去
-    const alreadyPreview =document.querySelector('.preview');
-    if (alreadyPreview) {
-      alreadyPreview.remove();
-    };
-    console.log("input要素で値が変化しました");
-    // input要素の中の画像を取得
-    console.log(e.target.files[0]);
-    const file = e.target.files[0]
-    // 画像データにアクセスできるURL作成
-    const blob = window.URL.createObjectURL(file);
-    console.log(blob);
+  
+  // プレビュー画像を生成・表示する関数
+  const buildPreviewImage = (dataIndex, blob) =>{
     // 画像を表示するためのdiv要素を生成
     const previewWrapper = document.createElement('div');
     previewWrapper.setAttribute('class', 'preview');
+    previewWrapper.setAttribute('data-index', dataIndex);
+
     // 表示する画像を生成
     const previewImage = document.createElement('img');
     previewImage.setAttribute('class', 'preview-image');
     previewImage.setAttribute('src',blob);
-    previewImage.setAttribute('width',112);
-    previewImage.setAttribute('height',112);
+
+    //削除ボタンを生成
+    const deleteButton = document.createElement('div');
+    deleteButton.setAttribute("class", "image-delete-button");
+    deleteButton.innerText = "削除";
+
+    // 削除ボタンをクリックしたらプレビューとfile_fieldを削除させる
+    deleteButton.addEventListener("click", () => deleteImage(dataIndex));
 
     // 生成したHTMLの要素をブラウザに表示させる
     previewWrapper.appendChild(previewImage);
+    previewWrapper.appendChild(deleteButton);
     previewList.appendChild(previewWrapper);
-  });
+  };
+
+  // file_fieldを生成・表示する関数
+  const buildNewFileField = () => {
+    // 2枚目用のfile_fieldを作成
+    const newFileField = document.createElement('input');
+    newFileField.setAttribute('type', 'file');
+    newFileField.setAttribute('name', 'item[images][]');
+
+    // 最後のfile_fieldを取得
+    const lastFileField = document.querySelector('input[type="file"][name="item[images][]"]:last-child');
+    // nextDataIndex = 最後のfile_fieldのdata-index + 1
+    const nextDataIndex = Number(lastFileField.getAttribute('data-index')) +1;
+    newFileField.setAttribute('data-index', nextDataIndex);
+
+    // 追加されたfile _fieldにイベントをセット
+    newFileField.addEventListener('change', changedFileField);
+
+    // 生成したfile_fieldを表示
+    const fileFieldsArea = document.querySelector('.click-upload');
+    fileFieldsArea.appendChild(newFileField);
+  };
+
+  // 指定したdata-indexを持つプレビューとfile_fieldを削除する
+  const deleteImage = (dataIndex) => {
+    const deletePreviewImage = document.querySelector(`.preview[data-index="${dataIndex}"]`)
+    deletePreviewImage.remove();
+    const deleteFileField = document.querySelector(`input[type="file"][data-index="${dataIndex}"]`)
+    deleteFileField.remove();
+  }
+
+  // input要素で値の変化が起きた際に呼び出される関数の中身
+  const changedFileField = (e) => {
+    // data-index(何番目を操作しているか)を取得
+    const dataIndex = e.target.getAttribute('data-index');
+
+    // input要素の中の画像を取得
+    const file = e.target.files[0]
+
+    // fileが空 = 何も選択しなかったのでプレビュー等を削除して終了する
+    if (!file) {
+      deleteImage(dataIndex);
+      return null;
+    }
+    // 画像データにアクセスできるURL作成
+    const blob = window.URL.createObjectURL(file);
+
+    // data-indexを使用して、すでにプレビューが表示されているか確認する
+    const alreadyPreview = document.querySelector(`.preview[data-index="${dataIndex}"]`);
+
+    if (alreadyPreview) {
+      // クリックしたfile＿fieldのdata-indexと、同じ番号のプレビュー画像がすでに表示されている場合は、画像の差し替えのみを行う
+      const alreadyPreviewImage = alreadyPreview.querySelector("img");
+      alreadyPreviewImage.setAttribute("src", blob);
+      return null;
+    }
+
+    buildPreviewImage(dataIndex, blob);
+    buildNewFileField();
+  };
+
+  // input要素を取得
+  const fileField = document.querySelector('input[type="file"][name="item[images][]"]');
+
+  // input要素に変化が起きた際に呼び出される関数
+  fileField.addEventListener('change', changedFileField);
 });
