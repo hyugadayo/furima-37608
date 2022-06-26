9d548d61e788ea00d77f473681f2bff6d063100a
hyugadayo <achano.0616@gmail.com>
2022-06-27 01:12:38 +0900

    コメント反映・表示

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,12 +2,12 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     if @comment.save
-      ActionCable.server.broadcast "comment_channel", {comment: @comment, useer: @comment.}
+      ActionCable.server.broadcast "comment_channel", {comment: @comment, user: @comment.user}
     end
   end
 
   private
-  def comment_param
+  def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id,item_id: params[:item_id])
   end
 end
--- app/javascript/channels/comment_channel.js
+++ app/javascript/channels/comment_channel.js
@@ -1,19 +1,27 @@
 import consumer from "./consumer"
 
 if(location.pathname.match(/\/items\/\d/)){
-  console.log("読み込み完了")
 
-consumer.subscriptions.create("CommentChannel", {
-  connected() {
-    // Called when the subscription is ready for use on the server
-  },
+  consumer.subscriptions.create("CommentChannel", {
+    connected() {
+      // Called when the subscription is ready for use on the server
+    },
 
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-  },
+    disconnected() {
+      // Called when the subscription has been terminated by the server
+    },
 
-  received(data) {
-    // Called when there's incoming data on the websocket for this channel
-  }
-});
+    received(data) {
+      console.log(data) //削除
+      const html = `
+        <div id="comment">
+          <p class="user-info">${data.user.nickname}： </p>
+          <p>${data.comment.text}</p>
+        </div>`
+      const commentArea = document.getElementById("comment-area")
+      commentArea.insertAdjacentHTML('beforeend', html)
+      const commentForm = document.getElementById("comment-form")
+      commentForm.reset();
+    }
+  })
 }
